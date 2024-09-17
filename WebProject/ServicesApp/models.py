36557fd55ffe53cce5725c53c0be832e76cc77ef
Virginia <viorbe20@gmail.com>
2024-09-17 12:33:24 +0200

    feat: create ServicesApp model

--- WebProject/ServicesApp/models.py
+++ WebProject/ServicesApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=100)
+    image = models.ImageField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    
+    # Defines model options that determines how Django handles the model at the database and admin level.
+    class Meta:
+        verbose_name = ('service') # Model´name that should be displayed in the admin level
+        verbose_name_plural = ('services') # Model name that should be displayed in the admin level
+    
+    def __str__(self):
+        return self.title
