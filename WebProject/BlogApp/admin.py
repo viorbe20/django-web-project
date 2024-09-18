abce61a832af218384120e599f29ce41b7e39585
Virginia <viorbe20@gmail.com>
2024-09-18 13:08:12 +0200

    feat: BlogApp admin config

--- WebProject/BlogApp/admin.py
+++ WebProject/BlogApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields=('createdAt','updatedAt')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=('createdAt','updatedAt')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
