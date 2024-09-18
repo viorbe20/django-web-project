c749d799c4c67f91f6c864b583eabd6042425f90
Virginia <viorbe20@gmail.com>
2024-09-18 18:51:19 +0200

    feat: posts per category view

--- WebProject/BlogApp/urls.py
+++ WebProject/BlogApp/urls.py
@@ -3,4 +3,5 @@ from BlogApp import views
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('categoria/<int:category_id>/', views.category, name="Categoria"),
 ]
--- WebProject/BlogApp/views.py
+++ WebProject/BlogApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from BlogApp.models import Post
+from BlogApp.models import Post, Category
 
 # Create your views here.
 
 def blog(request):
     all_posts = Post.objects.all()   
     return render(request, "BlogApp/blog.html", {"all_posts": all_posts})
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    posts_per_category = Post.objects.filter(categories=category)
+    all_posts = Post.objects.all()  
+    return render(request, "BlogApp/categories.html", {"category": category, "posts_per_category": posts_per_category, "all_posts": all_posts})
