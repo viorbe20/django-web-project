9c5edc5d6556d314b144032b6fe513fc25483146
Virginia <viorbe20@gmail.com>
2024-09-19 18:39:26 +0200

    feat: ContactApp save info form

--- WebProject/ContactApp/views.py
+++ WebProject/ContactApp/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 # Create your views here.
 
 def contact(request):
     contact_form = ContactForm()
+
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+        
+        if contact_form.is_valid():
+            name = request.POST.get('Nombre')
+            email = request.POST.get('Email')
+            message = request.POST.get('Mensaje')
+            
+            # Redirect to contact
+            
+            return redirect("/contacto/?valido")
+            
     return render(request, 'ContactApp/contact.html', {'contact_form': contact_form})
