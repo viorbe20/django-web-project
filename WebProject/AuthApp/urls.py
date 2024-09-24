4f4391c59eec871a875b261b1f3632180d06fa9c
Virginia <viorbe20@gmail.com>
2024-09-24 21:14:46 +0200

    feat: login form and user can login

--- WebProject/AuthApp/urls.py
+++ WebProject/AuthApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserAuthentication, user_logout
+from .views import UserAuthentication, user_logout, user_login
 
 urlpatterns = [
     path('', UserAuthentication.as_view(), name="Autenticacion"),
     path('cerrar_sesion', user_logout, name="CerrarSesion"),
+    path('iniciar_sesion', user_login, name="IniciarSesion"),
 ]
--- WebProject/AuthApp/views.py
+++ WebProject/AuthApp/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate
 
 class UserAuthentication(View):
     
     def get(self, request):
         form = UserCreationForm()
-        return render(request, 'AuthApp/authentication.html', {'form': form})
+        return render(request, 'Auth/authentication.html', {'form': form})
     
     def post(self, request):
         form = UserCreationForm(request.POST)
@@ -21,8 +22,28 @@ class UserAuthentication(View):
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
                 
-            return render(request, 'AuthApp/authentication.html', {'form': form})
+            return render(request, 'Auth/authentication.html', {'form': form})
     
 def user_logout(request):
     logout(request)
     return redirect('Inicio')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            user_name = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=user_name, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('Inicio')
+            else:
+                messages.error(request, "Usuario no válido")
+        else:
+            messages.error(request, "Información incorrecta")
+    
+    form = AuthenticationForm()
+    return render(request, 'Login/login.html', {'form': form})
