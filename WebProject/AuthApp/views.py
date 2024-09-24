1d09fbd9b0ccc7d229b0c0350cffa0adf1c57214
Virginia <viorbe20@gmail.com>
2024-09-24 15:06:37 +0200

    feat: user can log in

--- WebProject/AuthApp/views.py
+++ WebProject/AuthApp/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 class UserAuthentication(View):
     
     def get(self, request):
         form = UserCreationForm()
         return render(request, 'AuthApp/authentication.html', {'form': form})
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('Inicio')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+                
+            return render(request, 'AuthApp/authentication.html', {'form': form})
--- WebProject/CartApp/context_processor.py
+++ WebProject/CartApp/context_processor.py
@@ -1,12 +1,12 @@
 def total_cart_amount(request):
     total = 0
     
-    if request.user.is_authenticated:
-        # Get the cart of the session
-        cart = request.session.get("cart", {})
+    # if request.user.is_authenticated:
+    #     # Get the cart of the session
+    #     cart = request.session.get("cart", {})
         
-        # Add all the car items prizes
-        for key, value in request.session['cart'].items():
-            total = total + float(value['prize'])
+    #     # Add all the car items prizes
+    #     for key, value in request.session['cart'].items():
+    #         total = total + float(value['prize'])
         
-        return {"total_cart_amount": total}
+    return {"total_cart_amount": total}
